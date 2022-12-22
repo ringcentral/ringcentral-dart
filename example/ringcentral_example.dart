15f8fe3707d65178bed6e49f3652608cc3ac4ec2
Tyler Long <tyler4long@gmail.com>
2022-12-22 11:24:34 -0800

    First working password flow

--- example/ringcentral_example.dart
+++ example/ringcentral_example.dart
@@ -1,5 +1,19 @@
+import 'dart:io';
 import 'package:ringcentral/ringcentral.dart';
 
-void main() {
-  var rc = RingCentral();
+var env = Platform.environment;
+
+void main() async {
+  var rc = RingCentral(
+    env['RINGCENTRAL_SERVER_URL'] ?? '',
+    env['RINGCENTRAL_CLIENT_ID'] ?? '',
+    env['RINGCENTRAL_CLIENT_SECRET'] ?? '',
+  );
+  var r = await rc.authorize(
+    env['RINGCENTRAL_USERNAME'],
+    env['RINGCENTRAL_EXTENSION'],
+    env['RINGCENTRAL_PASSWORD'],
+  );
+  print(r.statusCode);
+  print(r.body);
 }
--- lib/src/ringcentral_base.dart
+++ lib/src/ringcentral_base.dart
@@ -1,1 +1,30 @@
-class RingCentral {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class RingCentral {
+  String server;
+  String clientId;
+  String clientSecret;
+
+  RingCentral(this.server, this.clientId, this.clientSecret) {
+    if (server.startsWith('https://')) {
+      server = server.substring(8);
+    }
+  }
+
+  Future authorize(username, extension, password) async {
+    var uri = Uri.https(server, 'restapi/oauth/token');
+    var basicAuth =
+        'Basic ${base64.encode(utf8.encode('$clientId:$clientSecret'))}';
+    var r = await http.post(uri, body: {
+      'grant_type': 'password',
+      'username': username,
+      'extension': extension,
+      'password': password,
+    }, headers: {
+      'authorization': basicAuth
+    });
+    return r;
+  }
+}
